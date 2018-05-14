91dee604504a2e6ec79e92432b0624f5429227de
Gopal <lakvas.gopal@gmail.com>
2018-05-14 17:48:11 +0530

    loginDemo
    login demo

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RouterModule, Routes} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {HttpModule} from "@angular/http";
+import { AppComponent } from './app.component';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+
+const appRoutes: Routes = [
+   {
+      path: '',
+      component: UserloginComponent
+   },
+   {
+      path: 'app-mainpage',
+      component: MainpageComponent
+   }
+];
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      UserloginComponent,
+      MainpageComponent
+   ],
+   
+   imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot(appRoutes),
+      BrowserAnimationsModule,
+      HttpModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
+})
+export class AppModule { }
